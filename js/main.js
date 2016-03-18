bbf1a851fbf68bca402461972c473bd368a01823
franchescamansour <cocomansour@Franchesca.local>
2016-03-18 16:31:18 -0400

    Created html css and js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $ball = $('.ball');
+
+var $html = $('html');
+
+$html.on('keydown', function () {
+
+  console.log(e.keyCode);
+  var div = document.createElement('div');
+  var rand = Math.floor(Math.random();
+});
